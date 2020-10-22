5ac19c50cd22afb8338adcb7842e316c418d5dce
KingAdam42 <61432177+KingAdam42@users.noreply.github.com>
2020-10-22 15:20:04 +0200

    Update jsbase.js

--- jsbase.js
+++ jsbase.js
@@ -1,2 +1,3 @@
 function GetAllItems() {
+  $.post("/page_resources/php/marketplace.remote.php", {type:"yang", action:"get_items", category_id:"9"}, function(d){$("#newcontainer").html(d);}, "html");
 }
